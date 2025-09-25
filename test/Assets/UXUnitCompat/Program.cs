3b4e6e27920aa0c2002b8e80d528e5769bbd18ea
Andy Gocke <andy@commentout.net>
2025-09-25 00:56:54 -0700

    Fix benchmark test (#4)

---
+++ test/Assets/UXUnitCompat/Program.cs
@@ -0,0 +1,9 @@
+class Program
+{
+    public static int Main()
+    {
+        Console.WriteLine("uxunit runner v0.0.1");
+        Console.WriteLine("  Discovering: XUnitCompat");
+        return 0;
+    }
+}
